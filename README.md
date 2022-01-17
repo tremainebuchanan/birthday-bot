ec33736e85583bd4fc0ccec842371bca7ec91111
tremainebuchanan <tremainekbuchanan@gmail.com>
2022-01-16 19:41:17 -0500

    adding license and readme

---
+++ README.md
@@ -0,0 +1,19 @@
+## Birthday Greeting Bot
+
+## Project Description
+
+** Birthday Greeting Bot** allows a user automate birthday greetings. 
+The script integrates with Whatsapp and once set up as a cron job will pull contacts from a file or database then send a custom birthday greeting. 
+
+## Language
+The main language used to develop this project was Python and the `pywhatkit` library. `pywhatkit` is a wrapper built to interact with the Whatsapp Web client.
+
+## Roadmap
+
+1. Build database model to represent contacts and messages
+2. Run the project from a web server
+3. Determine if a queue is required to deal with multiple messages to be 
+sent on a particular day.
+
+## License
+[MIT](LICENSE.md)
--- birthday_bot.py
+++ birthday_bot.py
@@ -8,20 +8,31 @@ import emoji
 def send_message(contact, message):
     pywhatkit.sendwhatmsg_instantly(contact, message, 15, True, 3)
 
-def readFile():
+def is_birthday_today(day, month):
     now = datetime.datetime.now()
-    n = datetime.datetime(now.year, now.month, now.day)
-    with open('contacts.csv', mode='r') as file:
-        csvFile = csv.DictReader(file)
-        for line in csvFile:
-            day = int(line['day'])
-            month = int(line['month'])
-            birthday = datetime.datetime(now.year, month, day)
-            if(n == birthday):
+    birthday = datetime.datetime(now.year, month, day)
+    today = datetime.datetime(now.year, now.month, now.day)
+    return today == birthday
+
+def start():
+    filename = "contacts.csv"
+    csvFile = False
+    try:
+        with open(filename, mode='r') as file:
+            csvFile = csv.DictReader(file)
+            for line in csvFile:
                 name = line['name']
-                message = emoji.emojize(f"Happy Happy Birthday {name}! :partying_face: :birthday_cake: :party_popper: :confetti_ball: :balloon:")
-                contact = line['phone']
-                send_message(contact,message)
-                
+                try:
+                    day = int(line['day'])
+                    month = int(line['month'])
+                    if is_birthday_today(day, month):
+                        message = emoji.emojize(f"Happy Happy Birthday {name}! :partying_face: :birthday_cake: :party_popper: :confetti_ball: :balloon:")
+                        contact = line['phone']
+                        send_message(contact,message)
+                except:
+                        print(f"Birthday for {name} was invalid")
+    except FileNotFoundError:
+        print(f"Unable to open {filename}")
+
 if __name__ == '__main__':
-    readFile()
+   start()
